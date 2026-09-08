4a20f2f662b1365c7448be3639f8720bf0acffee
BuddyBotRCE <cheggwin86@gmail.com>
2026-09-08 11:37:56 +0100

    em

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -223,13 +223,14 @@ module.exports = async (interaction, client) => {
             return await orpHandler(interaction, client);
         }
 
+        // 📢 EMBED BUILDER & REACTION PANEL ROUTER (THIS WAS MISSING!)
+        if (customId.startsWith('emb_') || customId.startsWith('select_emb_') || customId.startsWith('btn_emb_') || customId.startsWith('rr_') || customId.startsWith('select_rr_') || customId.startsWith('btn_rr_')) {
+            return await postEmbedHandler(interaction, client);
+        }
+
         if (customId.includes('kit') && !customId.includes('ticket')) {
             return await kitHandler(interaction, client);
         }
-        // 📢 Route Embed & Reaction Panel Builder Interactions
-if (customId.startsWith('emb_') || customId.startsWith('select_emb_') || customId.startsWith('btn_emb_') || customId.startsWith('rr_') || customId.startsWith('select_rr_') || customId.startsWith('btn_rr_')) {
-    return await postEmbedHandler(interaction, client);
-}
 
         return await adminHandler(interaction, client);
 
